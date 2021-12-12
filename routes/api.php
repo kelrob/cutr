ca4f0c4c402bc08896df89ed1219428a8e3cc4ca
ROBERT <tofa@Roberts-MacBook-Pro.local>
2021-12-12 19:24:46 +0100

    Modified commit

--- routes/api.php
+++ routes/api.php
@@ -11,8 +11,9 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
+| In
 |
-*/
+ */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
